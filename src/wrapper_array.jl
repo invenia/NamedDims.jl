79a45eab097947366e62d6a4143a3241c8564479
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-04-12 16:00:14 +0100

    more array stuff

--- src/wrapper_array.jl
+++ src/wrapper_array.jl
@@ -4,11 +4,19 @@ struct NamedDimsArray{L, T, N, A<:AbstractArray{T,N}} <: AbstractArray{T,N}
     data::A
 end
 
+function NamedDimsArray{L}(orig::AbstractArray{T,N}) where {L, T, N}
+    if !(L isa NTuple{N, Symbol})
+        throw(ArgumentError(
+            "A $N dimentional array, needs a $N-tuple of dimension names. Got: $L"
+        ))
+    end
+    return NamedDimsArray{L, T, N, typeof(orig)}(orig)
+end
 function NamedDimsArray(orig::AbstractArray{T,N}, names::NTuple{N, Symbol}) where {T, N}
-    return NamedDimsArray{names, T, N, typeof(orig)}(orig)
+    return NamedDimsArray{names}(orig)
 end
 
-
+parent_type(::Type{<:NamedDimsArray{L,T,N,A}}) where {L,T,N,A} = A
 Base.parent(x::NamedDimsArray) = x.data
 
 
@@ -29,9 +37,25 @@ name2dim(a::NamedDimsArray{L}, name) where L = name2dim(L, name)
 # AbstractArray Interface
 # https://docs.julialang.org/en/v1/manual/interfaces/index.html#man-interface-array-1
 
+## Minimal
 Base.size(a::NamedDimsArray) = size(parent(a))
-Base.getindex(A::NamedDimsArray, inds...) = getindex(parent(A), inds...)
-Base.setindex(A::NamedDimsArray, value, inds...) = setindex(parent(A), value, inds...)
+Base.size(a::NamedDimsArray, dim) = size(parent(a), name2dim(a, dim))
+
+Base.getindex(a::NamedDimsArray, inds...) = getindex(parent(a), inds...)
+Base.setindex!(a::NamedDimsArray, value, inds...) = setindex!(parent(a), value, inds...)
+
+## optional
+Base.IndexStyle(::Type{A}) where A<:NamedDimsArray = Base.IndexStyle(parent_type(A))
+
+Base.length(a::NamedDimsArray) = length(parent(a))
+
+Base.axes(a::NamedDimsArray) = axes(parent(a))
+Base.axes(a::NamedDimsArray, dim) = axes(parent(a), name2dim(a, dim))
+
+
+function Base.similar(a::NamedDimsArray{L}, args...) where L
+    return NamedDimsArray{L}(similar(parent(a), args...))
+end
 
 
 ###############################
@@ -63,7 +87,7 @@ function Base.getindex(A::NamedDimsArray; named_inds...)
 end
 
 
-function Base.setindex(A::NamedDimsArray, value; named_inds...)
+function Base.setindex!(A::NamedDimsArray, value; named_inds...)
     inds = order_named_inds(A; named_inds...)
-    return setindex(parent(A), value, inds...)
+    return setindex!(parent(A), value, inds...)
 end
--- test/wrapper_array.jl
+++ test/wrapper_array.jl
@@ -1,6 +1,6 @@
 using NamedDims
+using SparseArrays
 using Test
-
 
 @testset "get the parent array that was wrapped" begin
     orig = [1 2; 3 4]
@@ -21,5 +21,48 @@ end
 
     # Missing dims become slices
     @test nda[y=1] == nda[y=1, x=:] == nda[:, 1] == [10; 30]
+end
 
+@testset "setindex!" begin
+    @testset "by name" begin
+        nda = NamedDimsArray([10 20; 30 40], (:x, :y))
+
+        nda[x=1, y=1] = 100
+        @test nda == [100 20; 30 40]
+
+        # WORKWORK OUT WHY this is an error
+        #nda[x=1] .= 1000
+        #@test nda == [1000 1000; 30 40]
+    end
+
+    @testset "by position" begin
+        nda = NamedDimsArray([10 20; 30 40], (:x, :y))
+
+        nda[1, 1] = 100
+        @test nda == [100 20; 30 40]
+
+        nda[1,:] .= 1000
+        @test nda == [1000 1000; 30 40]
+    end
+end
+
+@testset "IndexStyle" begin
+    nda = NamedDimsArray([10 20; 30 40], (:x, :y))
+    @test IndexStyle(typeof(nda)) == IndexLinear()
+
+    sparse_nda = NamedDimsArray(spzeros(4,2), (:x, :y))
+    @test IndexStyle(typeof(sparse_nda)) == IndexCartesian()
+end
+
+
+@testset "length/size/axes" begin
+    nda = NamedDimsArray([10 20; 30 40; 50 60], (:x, :y))
+
+    @test length(nda) == 6
+
+    @test axes(nda) == (1:3, 1:2)
+    @test axes(nda,:x) == (1:3) == axes(nda,1)
+
+    @test size(nda) == (3,2)
+    @test size(nda,:x) == 3 == size(nda,1)
 end
