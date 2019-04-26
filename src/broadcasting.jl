cd98ebbe24107626839c7a348a9cf3c8fc40dee6
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-04-26 11:04:11 +0100

    import Broadcasting names

--- src/broadcasting.jl
+++ src/broadcasting.jl
@@ -1,37 +1,42 @@
 # See: https://docs.julialang.org/en/v1/manual/interfaces/#man-interfaces-broadcasting-1
 
+using Broadcast:
+    BroadcastStyle, DefaultArrayStyle, AbstractArrayStyle, Unknown,
+    Broadcasted, broadcasted
+
+
 """
     NamedDimsStyle{S}
 This is a `BroadcastStyle` for NamedDimsArray's
 It preserves the dimension names.
 `S` should be the `BroadcastStyle` of the wrapped type.
 """
-struct NamedDimsStyle{S <: Base.BroadcastStyle} <: Base.Broadcast.AbstractArrayStyle{Any} end
+struct NamedDimsStyle{S <: BroadcastStyle} <: AbstractArrayStyle{Any} end
 NamedDimsStyle(::S) where {S} = NamedDimsStyle{S}()
 NamedDimsStyle(::S, ::Val{N}) where {S,N} = NamedDimsStyle(S(Val(N)))
-NamedDimsStyle(::Val{N}) where N = NamedDimsStyle{Broadcast.DefaultArrayStyle{N}}()
-function NamedDimsStyle(a::Base.BroadcastStyle, b::Base.BroadcastStyle)
-    inner_style = Base.BroadcastStyle(a, b)
+NamedDimsStyle(::Val{N}) where N = NamedDimsStyle{DefaultArrayStyle{N}}()
+function NamedDimsStyle(a::BroadcastStyle, b::BroadcastStyle)
+    inner_style = BroadcastStyle(a, b)
 
-    # if the inner_style is Unknow then so is the outer-style
-    if inner_style isa Broadcast.Unknown
-        return Broadcast.Unknown()
+    # if the inner_style is Unknown then so is the outer-style
+    if inner_style isa Unknown
+        return Unknown()
     else
         return NamedDimsStyle(inner_style)
     end
 end
 function Base.BroadcastStyle(::Type{<:NamedDimsArray{L, T, N, A}}) where {L, T, N, A}
-    inner_style = typeof(Base.BroadcastStyle(A))
+    inner_style = typeof(BroadcastStyle(A))
     return NamedDimsStyle{inner_style}()
 end
 
 function Base.BroadcastStyle(::NamedDimsStyle{A}, ::NamedDimsStyle{B}) where {A, B}
     return NamedDimsStyle(A(), B())
 end
-function Base.BroadcastStyle(::NamedDimsStyle{A}, b::B) where {A, B <: Broadcast.BroadcastStyle}
+function Base.BroadcastStyle(::NamedDimsStyle{A}, b::B) where {A, B <: BroadcastStyle}
     return NamedDimsStyle(A(), b)
 end
-function Base.BroadcastStyle(a::A, ::NamedDimsStyle{B}) where {A <: Broadcast.BroadcastStyle, B}
+function Base.BroadcastStyle(a::A, ::NamedDimsStyle{B}) where {A <: BroadcastStyle, B}
     return NamedDimsStyle(a, B())
 end
 
@@ -39,9 +44,9 @@ end
 
 function Broadcast.broadcasted(::NamedDimsStyle{S}, f, args...) where S
     # Delgate to inner style
-    inner = Broadcast.broadcasted(S(), f, args...)
-    if inner isa Broadcast.Broadcasted
-        return Broadcast.Broadcasted{NamedDimsStyle{S}}(inner.f, inner.args, inner.axes)
+    inner = broadcasted(S(), f, args...)
+    if inner isa Broadcasted
+        return Broadcasted{NamedDimsStyle{S}}(inner.f, inner.args, inner.axes)
     else # eagerly evaluated
         return inner
     end
@@ -49,10 +54,10 @@ end
 
 
 function Base.similar(
-    bc::Broadcast.Broadcasted{NamedDimsStyle{S}},
+    bc::Broadcasted{NamedDimsStyle{S}},
     ::Type{T}
 ) where {S,T}
-    inner_bc = Broadcast.Broadcasted{S}(bc.f, bc.args, bc.axes)
+    inner_bc = Broadcasted{S}(bc.f, bc.args, bc.axes)
     data = similar(inner_bc, T)
 
     L = broadcasted_names(bc)
@@ -60,7 +65,7 @@ function Base.similar(
 end
 
 
-broadcasted_names(bc::Broadcast.Broadcasted) = broadcasted_names(bc.args...)
+broadcasted_names(bc::Broadcasted) = broadcasted_names(bc.args...)
 function broadcasted_names(a, bs...)
     a_name = broadcasted_names(a)
     b_name = broadcasted_names(bs...)
@@ -85,7 +90,7 @@ end
 
 
 function Base.similar(
-    bc::Broadcast.Broadcasted{TrackedStyle},
+    bc::Broadcasted{TrackedStyle},
     ::Type{TrackedReal{T}}
 ) where {S,T}
     # This is not great, as it loose what type Tracker was wrapping
