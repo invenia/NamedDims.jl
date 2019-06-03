9af15ebb7eb03fc7e468562275ad0078d6b72360
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-06-03 17:59:57 +0100

    Add QR

--- src/functions_linearalgebra.jl
+++ src/functions_linearalgebra.jl
@@ -29,14 +29,14 @@ end
 function Base.getproperty(fact::LU{T,<:NamedDimsArray{L}}, d::Symbol) where {T, L}
     inner = getproperty(parent(fact), d)
     n1, n2 = L
-    if d == :L
+    if d === :L
         return NamedDimsArray{(n1, :_)}(inner)
-    elseif d == :U
+    elseif d === :U
         return NamedDimsArray{(:_, n2)}(inner)
-    elseif d == :P
+    elseif d === :P
         perm_matrix_labels = (first(L), first(L))
         return NamedDimsArray{perm_matrix_labels}(inner)
-    elseif d == :p
+    elseif d === :p
         perm_vector_labels = (first(L),)
         return NamedDimsArray{perm_vector_labels}(inner)
     else
@@ -64,9 +64,9 @@ end
 function Base.getproperty(fact::LQ{T,<:NamedDimsArray{L}}, d::Symbol) where {T, L}
     inner = getproperty(parent(fact), d)
     n1, n2 = L
-    if d == :L
+    if d === :L
         return NamedDimsArray{(n1, :_)}(inner)
-    elseif d == :Q
+    elseif d === :Q
         return NamedDimsArray{(:_, n2)}(inner)
     else
         return inner
@@ -101,13 +101,64 @@ end
 function Base.getproperty(fact::SVD{T, Tr, <:NamedDimsArray{L}}, d::Symbol) where {T, Tr, L}
     inner = getproperty(parent(fact), d)
     n1, n2 = L
-    if d == :U
+    if d === :U
         return NamedDimsArray{(n1,:_)}(inner)
-    elseif d == :V
+    elseif d === :V
         return NamedDimsArray{(:_, n2)}(inner)
-    elseif d == :Vt
+    elseif d === :Vt
         return NamedDimsArray{(n2,:_)}(inner)
     else # :S
+        return inner
+    end
+end
+
+##
+# qr
+
+function LinearAlgebra.qr!(a::NamedDimsArray{L}, args...; kwargs...) where L
+    inner = qr!(parent(a), args...; kwargs...)
+    return _named_qr(L, inner)
+end
+
+_named_qr(L, inner::QR) = QR(NamedDimsArray{L}(inner.factors), inner.τ)
+function Base.parent(fact::QR{<:Any, <:NamedDimsArray})
+    factors = parent(getfield(fact, :factors))
+    τ = getfield(fact, :τ)
+    return QR(factors, τ)
+end
+
+_named_qr(L, inner::LinearAlgebra.QRCompactWY) = LinearAlgebra.QRCompactWY(NamedDimsArray{L}(inner.factors), inner.T)
+function Base.parent(fact::LinearAlgebra.QRCompactWY{<:Any, <:NamedDimsArray})
+    factors = parent(getfield(fact, :factors))
+    T = getfield(fact, :T)
+    return LinearAlgebra.QRCompactWY(factors, T)
+end
+
+_named_qr(L, inner::QRPivoted) = QRPivoted(NamedDimsArray{L}(inner.factors), inner.τ, inner.jpvt)
+function Base.parent(fact::QRPivoted{<:Any, <:NamedDimsArray})
+    factors = parent(getfield(fact, :factors))
+    τ = getfield(fact, :τ)
+    jpvt = getfield(fact, :jpvt)
+    return QRPivoted(factors, τ, jpvt)
+end
+
+const NamedQR{L} = Union{
+    QR{<:Any, <:NamedDimsArray{L}},
+    LinearAlgebra.QRCompactWY{<:Any, <:NamedDimsArray{L}},
+    QRPivoted{<:Any, <:NamedDimsArray{L}},
+}
+function Base.getproperty(fact::NamedQR{L}, d::Symbol) where {L}
+    inner = getproperty(parent(fact), d)
+    n1, n2 = L
+    if d === :Q
+        return NamedDimsArray{(n1,:_)}(inner)
+    elseif d === :R
+        return NamedDimsArray{(:_, n2)}(inner)
+    elseif fact isa QRPivoted && d === :P
+        return NamedDimsArray{(n1, n1)}(inner)
+    elseif fact isa QRPivoted && d === :p
+        return NamedDimsArray{(n1,)}(inner)
+    else
         return inner
     end
 end
--- test/functions_linearalgebra.jl
+++ test/functions_linearalgebra.jl
@@ -1,3 +1,7 @@
+using LinearAlgebra
+using NamedDims
+using NamedDims: names
+using Test
 
 # LinearAlgebra
 @testset "lu" begin
@@ -34,4 +38,30 @@ end
 
     # Identity operation should give back original nam,es
     @test names(x.U * Diagonal(x.S) * x.V) == (:foo, :bar)
+end
+
+@testset "qr" begin
+    for pivot in (true,false)
+        for data in ([1.0 2; 3 4], [big"1.0" 2; 3 4])
+            nda = NamedDimsArray{(:foo, :bar)}(data)
+            x = qr(nda, Val(pivot));
+            @test names(x.Q) == (:foo, :_)
+            @test names(x.R) == (:_, :bar)
+
+            # Identity operation should give back original names
+            @test names(x.Q * x.R) == (:foo, :bar)
+
+            pivot && @testset "pivoted" begin
+                @test x isa QRPivoted
+                @test names(x.p) == (:foo,)
+                @test names(x.P) == (:foo, :foo)
+
+                # Identity operation should give back original names
+                @test names(x.P * nda) == (:foo, :bar)
+                @test names(nda[x.p, :]) == (:foo, :bar)
+            end
+        end
+    end
+
+
 end
