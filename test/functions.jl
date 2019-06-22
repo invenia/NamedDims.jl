de99567d8de56d925bb8ee7b110b5e4a785d9bc8
Nick Robinson <npr251@gmail.com>
2019-06-22 18:08:50 +0100

    Test prod, maximum, minimum, extrema

--- test/functions.jl
+++ test/functions.jl
@@ -3,32 +3,20 @@ using NamedDims: names
 using Test
 using Statistics
 
-@testset "sum" begin
-    a = [10 20; 30 40]
-    nda = NamedDimsArray(a, (:x, :y))
+@testset "Base" begin
+a = [10 20; 31 40]
+nda = NamedDimsArray(a, (:x, :y))
 
-    @test sum(nda) == 100 == sum(a)
-    @test sum(nda; dims=:x) == [40 60] == sum(nda; dims=1) == sum(a; dims=1)
+@testset "$f" for f in (sum, prod, maximum, minimum, extrema)
+    @test f(nda) == f(a)
+    @test f(nda; dims=:x) == f(nda; dims=1) == f(a; dims=1)
 
-    @test names(sum(nda; dims=:x)) == (:x, :y) == names(sum(nda; dims=1))
-end
-
-@testset "mean" begin
-    a = [10 20; 30 40]
-    nda = NamedDimsArray(a, (:x, :y))
-
-    @test mean(nda) == 25 == mean(a)
-    @test mean(nda; dims=:x) == [20 30] == mean(nda; dims=1) == mean(a; dims=1)
-
-    @test names(mean(nda; dims=:x)) == (:x, :y) == names(mean(nda; dims=1))
+    @test names(f(nda; dims=:x)) == (:x, :y) == names(f(nda; dims=1))
 end
 
 @testset "mapslices" begin
-    a = [10 20; 30 40]
-    nda = NamedDimsArray(a, (:x, :y))
-
-    @test mapslices(join, nda; dims=:x) == ["1030" "2040"] == mapslices(join, nda; dims=1)
-    @test mapslices(join, nda; dims=:y) == reshape(["1020", "3040"], Val(2)) == mapslices(join, nda; dims=2)
+    @test mapslices(join, nda; dims=:x) == ["1031" "2040"] == mapslices(join, nda; dims=1)
+    @test mapslices(join, nda; dims=:y) == reshape(["1020", "3140"], Val(2)) == mapslices(join, nda; dims=2)
     @test_throws UndefKeywordError mapslices(join, nda)
     @test_throws UndefKeywordError mapslices(join, a)
 
@@ -36,8 +24,6 @@ end
 end
 
 @testset "mapreduce" begin
-    a = [10 20; 31 40]
-    nda = NamedDimsArray(a, (:x, :y))
     @test mapreduce(isodd, |, nda; dims=:x) == [true false] == mapreduce(isodd, |, nda; dims=1)
     @test mapreduce(isodd, |, nda; dims=:y) == [false true]' == mapreduce(isodd, |, nda; dims=2)
     @test mapreduce(isodd, |, nda) == true == mapreduce(isodd, |, a)
@@ -46,8 +32,6 @@ end
 end
 
 @testset "zero" begin
-    a = [10 20; 31 40]
-    nda = NamedDimsArray(a, (:x, :y))
     @test zero(nda) == [0 0; 0 0] == zero(a)
     @test names(zero(nda)) == (:x, :y)
 end
