e52c9c1798df21fee3d664ead7826764b377a192
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2019-08-22 23:27:20 -0400

    tidy + *(i,j) etc

--- src/macros.jl
+++ src/macros.jl
@@ -3,11 +3,6 @@ export @named, @unname
 using MacroTools
 
 """
-    A′ = @named A[i,j,k]
-
-Macro for adding dimension names, or asserting that they agree.
-The same as calling `NamedDimsArray(A, (:i, :j, :k))`.
-
     @named begin
         A′ = A[i,j,k]
         B′ = B[k]
@@ -15,16 +10,23 @@ The same as calling `NamedDimsArray(A, (:i, :j, :k))`.
         f(A′, C) .+ B′
     end
 
-When applied to a block of code, `A′` and `B′` will be defined, and it wil be asserted
+Convenience macro for adding dimension names, or asserting that they agree.
+The same as calling `A′ = NamedDimsArray(A, (:i, :j, :k))`. Here it wil be asserted
 that `C` is a 3-tensor whose dimension names (if any) are compatible with `(:i, :_, :k)`.
 Other expressions like `f` are run as usual.
 
-    @named *ⱼ = *(j)
+    @named begin
+        *′ = *(i)
+        *ᵢⱼ = *(i,j)
+        /ⱼ = /(j)
+    end
 
-This defines a function `*ⱼ` which multiplies two `NamedDimsArray`s always along index `j`.
-The function may have any name, but decorations of `*` such as `*′` or `*ⱼ` give an
-infix operator which may be used `A′ *ⱼ D`.
-Like `sum(A′, dims=:k)` this will error on ordinary arrays.
+This defines a function `*′` which multiplies two `NamedDimsArray`s always along index `i`.
+(You will need the package `OMEinsum` for 3-index and higher arrays, or to sum over multiple indices.)
+The function may have any name, but decorations of `*` such as `*′` or `*ᵢⱼ` give an
+infix operator which may be used `A′ *′ D`.
+Similarly `/ⱼ` is like `/` but transposes as needed to act on shared index `j`.
+(Soon!)
 """
 macro named(ex)
     named_macro(ex)
@@ -37,7 +39,12 @@ function named_macro(input_ex)
             return :( NamedDims.NamedDimsArray{($(QuoteNode.(ijk)...),)}($A) )
 
         elseif @capture(ex, s_ = *(i_) )
-            return :( $s(xs...) = *($(QuoteNode(i)), xs...) )
+            return :( $s(xs...) = Base.:*($(QuoteNode(i)), xs...) )
+        elseif @capture(ex, s_ = *(ijk__) )
+            return :( $s(xs...) = Base.:*(($(QuoteNode.(ijk)...),), xs...) )
+
+        elseif @capture(ex, s_ = /(i_) )
+            return :( $s(x,y) = Base.:/($(QuoteNode(i)), x,y) )
 
         end
         return ex
@@ -50,6 +57,7 @@ end
 
 Macro for removing dimension names. If `A == @named A[i,j,k]` then this returns `parent(A)`,
 but if `A` has these names in another order, then it returns `permutedims(parent(A), (:i,:j,:k))`.
+@named
 """
 macro unname(ex)
     unname_macro(ex)
