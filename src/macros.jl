999a1d2a34f2efee50dbc9ea63b16c409238b42b
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2019-08-25 22:47:40 -0400

    new

--- src/macros.jl
+++ src/macros.jl
@@ -1,75 +1,66 @@
-export @named, @unname
+export @namedef
 
 using MacroTools
 
 """
-    @named begin
-        A′ = A[i,j,k]
-        B′ = B[k]
-        C[i,_,k]
-        f(A′, C) .+ B′
+    @namedef begin
+        A => A′{i,j}
+        B′{i,j} => B
+        C′{i,j} => C′′{x,y}
+        contract => *′{i}
     end
+    A′ = @namedef A => {i,j}
 
-Convenience macro for adding dimension names, or asserting that they agree.
-The same as calling `A′ = NamedDimsArray(A, (:i, :j, :k))`. Here it wil be asserted
-that `C` is a 3-tensor whose dimension names (if any) are compatible with `(:i, :_, :k)`.
-Other expressions like `f` are run as usual.
-
-    @named begin
-        *′ = *(i)
-        *ᵢⱼ = *(i,j)
-        /ⱼ = /(j)
-    end
-
-This defines a function `*′` which multiplies two `NamedDimsArray`s always along index `i`.
-(You will need the package `OMEinsum` for 3-index and higher arrays, or to sum over multiple indices.)
-The function may have any name, but decorations of `*` such as `*′` or `*ᵢⱼ` give an
-infix operator which may be used `A′ *′ D`.
-Similarly `/ⱼ` is like `/` but transposes as needed to act on shared index `j`.
-(Soon!)
+Macro for adding and removing dimension names.
+`A′ = NamedDimsArray{(:i, :j)}(A)` has the same data as `A` but its type contains `i,j`.
+It is asserted that `B′` has names `i,j`, and this is unwrapped to array `B` in this order.
+`C′′` is a re-named version of `C′`.
+And `*′` is an infix contraction function along index `j`.
 """
-macro named(ex)
-    named_macro(ex)
+macro namedef(ex)
+    names_macro(ex)
 end
 
-function named_macro(input_ex)
-    outex = MacroTools.prewalk(input_ex) do ex
+function names_macro(input_ex)
+    outex = quote end
+    if input_ex.head == :block
+        for ex in input_ex.args
+            ex isa LineNumberNode && continue
 
-        if @capture(ex, A_[ijk__])
-            return :( NamedDims.NamedDimsArray{($(QuoteNode.(ijk)...),)}($A) )
+            # Special words contract etc must come first
+            if @capture(ex, contract => g_{i_})
+                push!(outex.args, :( $g(xs...) = Base.:*($(QuoteNode(i)), xs...) ))
 
-        elseif @capture(ex, s_ = *(i_) )
-            return :( $s(xs...) = Base.:*($(QuoteNode(i)), xs...) )
-        elseif @capture(ex, s_ = *(ijk__) )
-            return :( $s(xs...) = Base.:*(($(QuoteNode.(ijk)...),), xs...) )
+            # Then conversion of arrays etc
+            elseif @capture(ex, A_ => B_{ijk__})
+                stup = :( ($(QuoteNode.(ijk)...),) )
+                push!(outex.args, :( $B = NamedDims.NamedDimsArray($A, $stup) ))
 
-        elseif @capture(ex, s_ = /(i_) )
-            return :( $s(x,y) = Base.:/($(QuoteNode(i)), x,y) )
+            elseif @capture(ex, C_{ijk__} => D_)
+                stup = :( ($(QuoteNode.(ijk)...),) )
+                # push!(outex.args, :( $D = NamedDims.unname($C, $stup) ))
+                push!(outex.args, :( $D = NamedDims.unname(Base.permutedims($C, $stup)) ))
 
+            elseif @capture(ex, E_{ijk__} => F_{xyz__})
+                stup = :( ($(QuoteNode.(ijk)...),) )
+                stup2 = :( ($(QuoteNode.(xyz)...),) )
+                push!(outex.args, :( $F = NamedDims.NamedDimsArray(NamedDims.unname(Base.permutedims($C, $stup)), $stup2) ))
+
+            else
+                error("@namedef doesn't know what to do with $ex")
+            end
         end
-        return ex
+    else
+        if @capture(input_ex, A_ => {ijk__})
+            @gensym B
+            return names_macro(quote
+                $A => $B{$(ijk...)}
+            end)
+        else
+            return names_macro(quote
+                $input_ex
+            end)
+        end
     end
     esc(outex)
-end
-
-"""
-    @unname A[i,j,k]
-
-Macro for removing dimension names. If `A == @named A[i,j,k]` then this returns `parent(A)`,
-but if `A` has these names in another order, then it returns `permutedims(parent(A), (:i,:j,:k))`.
-@named
-"""
-macro unname(ex)
-    unname_macro(ex)
-end
-
-function unname_macro(input_ex)
-    outex = MacroTools.prewalk(input_ex) do ex
-        if @capture(ex, A_[ijk__])
-            stup = :( ($(QuoteNode.(ijk)...),) )
-            return :( NamedDims.names($A) == $stup ? parent($A) : parent(permutedims($A, $stup)) )
-        end
-
-        return ex
-    end
 end
