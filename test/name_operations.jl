6682e519ac08efaa9071a442a455ba764aee0df2
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2020-02-24 18:58:02 +0100

    add permutedims example to tests

--- test/name_operations.jl
+++ test/name_operations.jl
@@ -6,7 +6,7 @@
 end
 
 
-@testset "dimnames" begin
+@testset "dimnames" begin   
     nda = NamedDimsArray([10 20; 30 40], (:x, :y))
 
     @test dimnames(nda) === (:x, :y)
@@ -16,6 +16,9 @@ end
     @test dimnames([10 20; 30 40]) === (:_, :_)
     @test dimnames([10 20; 30 40], 2) === :_
     @test dimnames([10 20; 30 40], 3) === :_
+    
+    v = NamedDimsArray(1:2, :a)
+    @test dimnames(v, 2) == dimnames(permutedims(v), 1) # That's why :_ for d > ndims
 
     @test_throws Exception dimnames(nda, 0)
 end
