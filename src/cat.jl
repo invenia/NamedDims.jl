efa29a29b3267ff56f53adc02d47679e8231df5b
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2020-08-23 18:36:19 +0100

    make hcat and vcat more efficient for more than two arguments

--- src/cat.jl
+++ src/cat.jl
@@ -7,6 +7,8 @@ function Base.cat(a::NamedDimsArray{L}; dims) where L
     return NamedDimsArray{newL, T, N, Array{T,N}}(data)
 end
 
+# While the following two functions are covered by the general case below where splatted c
+# is empty, they are included because they reduce allocations to that of regular arrays.
 function Base.cat(a::NamedDimsArray{L}, b::AbstractArray; dims) where L
     newL = expand_dimnames(L, dims)
     numerical_dims = dim(newL, dims)
@@ -25,7 +27,7 @@ function Base.cat(a::AbstractArray, b::NamedDimsArray{L}; dims) where L
     return NamedDimsArray{newL, T, N, Array{T,N}}(data)
 end
 
-# to dispatch on the first or the second argument being the NDA
+# to dispatch on the first _or the second_ argument being the NDA.
 for (T, S) in [
     (:NamedDimsArray, :AbstractArray),
     (:AbstractArray, :NamedDimsArray),
@@ -35,7 +37,7 @@ for (T, S) in [
     @eval function Base.cat(a::$T, b::$S, c::AbstractArray...; dims)
         combL = unify_names_shortest(dimnames(a), dimnames(b), ntuple(i->dimnames(c[i]), length(c))...)
         newL = expand_dimnames(combL, dims)
-        numerical_dims = dim(combL, dims)
+        numerical_dims = dim(newL, dims)
         data = Base.cat(parent(a), parent(b), ntuple(i->parent(c[i]), length(c))...; dims=numerical_dims)
         T = promote_type(eltype(a), eltype(b), ntuple(i->eltype(c[i]), length(c))...)
         N = length(newL)
@@ -65,6 +67,7 @@ for (T, S) in [
     ]
 
     for (fun, d) in zip((:vcat, :hcat), (1, 2))
+        # this special case is needed to resolve ambiguities
         @eval function Base.$fun(a::$T, b::$S)
             combL = unify_names_shortest(dimnames(a), dimnames(b))
             newL = expand_dimnames(combL, $d)
@@ -75,7 +78,12 @@ for (T, S) in [
         end
 
         @eval function Base.$fun(a::$T, b::$S, c::NamedDimsArray...)
-            return Base.cat(a, b, c...; dims=$d)
+            combL = unify_names_shortest(dimnames(a), dimnames(b), ntuple(i->dimnames(c[i]), length(c))...)
+            newL = expand_dimnames(combL, $d)
+            data = Base.$fun(parent(a), parent(b), ntuple(i->parent(c[i]), length(c))...)
+            T = promote_type(eltype(a), eltype(b), ntuple(i->eltype(c[i]), length(c))...)
+            N = length(newL)
+            return NamedDimsArray{newL, T, N, Array{T,N}}(data)
         end
     end
 end
