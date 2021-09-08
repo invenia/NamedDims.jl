3b4eaeb51f9123a2b6e86b158b8352a2a0d33592
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-09-08 12:29:34 +0100

    only check inferrence on 1.6 and later

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -1,4 +1,4 @@
 @testset "chainrules.jl" begin
-    test_rrule(NamedDimsArray, rand(2, 3), (:a, :b))
-    test_rrule(NamedDimsArray{(:a, :b)}, (rand(2, 3)))
+    test_rrule(NamedDimsArray, rand(2, 3), (:a, :b); check_inferred=VERSION >= v"1.6")
+    test_rrule(NamedDimsArray{(:a, :b)}, (rand(2, 3)); check_inferred=VERSION >= v"1.6")
 end
