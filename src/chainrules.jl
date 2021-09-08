240be2dd2e6a6355fcf591ae39a2f6d9b90d3fa1
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-09-08 12:05:24 +0100

    add rrule for constructor with values only

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,7 +1,11 @@
 _NamedDimsArray_pullback(ȳ::AbstractArray) = (NoTangent(), ȳ, NoTangent())
 _NamedDimsArray_pullback(ȳ::Tangent) = (NoTangent(), ȳ.data, NoTangent())
 _NamedDimsArray_pullback(ȳ::AbstractThunk) = _NamedDimsArray_pullback(unthunk(ȳ))
-
-function ChainRulesCore.rrule(::Type{NamedDimsArray}, values, names)
+function ChainRulesCore.rrule(::Type{<:NamedDimsArray}, values, names)
     return NamedDimsArray(values, names), _NamedDimsArray_pullback
+end
+
+function ChainRulesCore.rrule(T::Type{<:NamedDimsArray}, values)
+    NamedDimsArray_values_pullback(ȳ) = _NamedDimsArray_pullback(ȳ)[1:2]
+    return T(values), NamedDimsArray_values_pullback
 end
--- test/chainrules.jl
+++ test/chainrules.jl
@@ -1,3 +1,4 @@
 @testset "chainrules.jl" begin
     test_rrule(NamedDimsArray, rand(2, 3), (:a, :b))
+    test_rrule(NamedDimsArray{(:a, :b)}, (rand(2, 3)))
 end
