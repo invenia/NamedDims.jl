d1c14dd8d426a01f49dd8deccba27dffad3a0633
John Omotani <john.omotani@ukaea.uk>
2021-11-26 11:31:23 +0000

    Skip broadcast const-propagation test for Julia<=1.3
    Test for allocations fails on Julia-1.3, but passes on newer versions.
    Just skip the test for versions that are too old.

--- test/broadcasting.jl
+++ test/broadcasting.jl
@@ -149,11 +149,13 @@ function foo(a, b, c, d)
     @. a -= 0.5 * d * b * c
     return a
 end
-@testset "Regression test against #187 (allocations)" begin
-    # # https://github.com/invenia/NamedDims.jl/issues/187
-    a = NamedDimsArray{(:z,)}(rand(5))
-    b = NamedDimsArray{(:z,)}(rand(5))
-    c = rand()
-    d = rand()
-    @test 0 == @ballocated foo($a, $b, $c, $d)
+if VERSION >= v"1.4"
+    @testset "Regression test against #187 (allocations)" begin
+        # # https://github.com/invenia/NamedDims.jl/issues/187
+        a = NamedDimsArray{(:z,)}(rand(5))
+        b = NamedDimsArray{(:z,)}(rand(5))
+        c = rand()
+        d = rand()
+        @test 0 == @ballocated foo($a, $b, $c, $d)
+    end
 end
