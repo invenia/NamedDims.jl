2aea7bd41459a60a7f70a4e787e3e3406b8299a5
Miha Zgubic <mzgubic@users.noreply.github.com>
2022-08-05 12:25:26 +0100

    implement `ProjectTo` for `NamedDimsArray` (#201)
    * implement ProjectTo for NamedDims
    
    * implement name matching
    
    * test notangent

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -1,4 +1,44 @@
 @testset "chainrules.jl" begin
-    test_rrule(NamedDimsArray, rand(2, 3), (:a, :b); check_inferred=VERSION >= v"1.6")
-    test_rrule(NamedDimsArray{(:a, :b)}, (rand(2, 3)); check_inferred=VERSION >= v"1.6")
+    @testset "constructor" begin
+        test_rrule(NamedDimsArray, rand(2, 3), (:a, :b); check_inferred=VERSION >= v"1.6")
+        test_rrule(NamedDimsArray{(:a, :b)}, (rand(2, 3)); check_inferred=VERSION >= v"1.6")
+    end
+
+    @testset "ProjectTo" begin
+        nda = NamedDimsArray{(:a, :b)}(rand(3, 3))
+
+        @testset "NoTangent()" begin
+            @test NoTangent() == ProjectTo(nda)(NoTangent())
+        end
+
+        @testset "(:c, :d) -> (:a, :b) == error" begin
+            ndb = NamedDimsArray{(:c, :d)}(rand(3, 3))
+            @test_throws DimensionMismatch ProjectTo(nda)(ndb)
+        end
+
+        @testset "(:_, :_) -> (:a, :b) == (:a, :b)" begin
+            x = rand(3, 3)
+            projected = @inferred ProjectTo(nda)(x)
+            @test dimnames(projected) == dimnames(nda)
+        end
+
+        @testset "(:a, :_) -> (:a, :b) == (:a, :b)" begin
+            nd1 = NamedDimsArray{(:a, :_)}(rand(3, 3))
+            projected = @inferred ProjectTo(nda)(nd1)
+            @test dimnames(projected) == dimnames(nda)
+        end
+
+        @testset "(:a, :b) -> (:a, :_) == (:a, :b)" begin
+            nd1 = NamedDimsArray{(:a, :_)}(rand(3, 3))
+            projected = @inferred ProjectTo(nd1)(nda) # switched order compared to above
+            @test dimnames(projected) == dimnames(nda)
+        end
+
+        @testset "(:_, :b) -> (:a, :_) == (:a, :b)" begin
+            nd1 = NamedDimsArray{(:a, :_)}(rand(3, 3))
+            nd2 = NamedDimsArray{(:_, :b)}(rand(3, 3))
+            projected = @inferred ProjectTo(nd1)(nd2)
+            @test dimnames(projected) == (:a, :b)
+        end
+    end
 end
